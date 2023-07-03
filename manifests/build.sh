6c1557dbabfa95ebda244a58878ffd9614d9d029
fafnirZ <gokurohono@gmail.com>
2023-07-03 22:56:44 +1000

    chore: minor manifest fixes

--- manifests/build.sh
+++ manifests/build.sh
@@ -7,5 +7,6 @@ touch main.yaml
 # concatenates all manifests inside independent/ into a single file
 for i in independent/*.yaml; do
   cat $i >> main.yaml
+  echo "\n" >> main.yaml # ensures whitespace
   echo "---" >> main.yaml
 done
